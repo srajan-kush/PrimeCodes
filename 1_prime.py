f4131390dc8c7bc4cdc896471629d82f51049705
srajan-kush <144719261+srajan-kush@users.noreply.github.com>
2023-09-12 20:44:39 +0530

    updated

--- 1_prime.py
+++ 1_prime.py
@@ -2,10 +2,10 @@ m = int(input("Enter a number: "))
 i=2
 while i<m :
     if m%i ==0 :
-        print("NOT PRIME")
+        print("NOT PRIME NUMBER")
         break
     else:
         i=i+1
 if i==m :
-    print("PRIME")
+    print("PRIME NUMBER")
 
